ad18edd1f1258182f14f14eb83196d469f9315df
NicolasC <nicolas11072@gmail.com>
2026-07-27 22:43:32 -0400

    Agregar estados para los campos del formulario de mascota

--- src/components/MascotaForm.jsx
+++ src/components/MascotaForm.jsx
@@ -3,6 +3,16 @@ import { useNavigate } from "react-router-dom";
 import api from "../services/api";
 
 function MascotaForm() {
+  const [nombre, setNombre] = useState("");
+  const [descripcion, setDescripcion] = useState("");
+  const [imagen, setImagen] = useState(null);
+  const [estado, setEstado] = useState("");
+  const [tipoAnimal, setTipoAnimal] = useState("");
+  const [edad, setEdad] = useState("");
+  const [raza, setRaza] = useState("");
+  const [sexo, setSexo] = useState("");
+  const [tamano, setTamano] = useState("");
+
   return (
     <div>
       <h1>Registrar mascota</h1>
