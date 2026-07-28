c9459cd73ac011810eaecf859c03e2cb7ca60490
NicolasC <nicolas11072@gmail.com>
2026-07-27 22:42:40 -0400

    Agregar importaciones y dependencias de MascotaForm

---
+++ src/components/MascotaForm.jsx
@@ -0,0 +1,13 @@
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import api from "../services/api";
+
+function MascotaForm() {
+  return (
+    <div>
+      <h1>Registrar mascota</h1>
+    </div>
+  );
+}
+
+export default MascotaForm;
