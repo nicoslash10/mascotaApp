36b1c14a3cb233161d57369bd4c4cf12b4904d18
NicolasC <nicolas11072@gmail.com>
2026-07-27 22:12:44 -0400

    feat: configurar rutas principales de la aplicación

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,24 @@
-
+import NavBar from "./components/NavBar";
+import MascotaPage from "./pages/MascotaPage";
 
 function App() {
-  
+  return (
+    <>
+      <NavBar />
+
+      <div className="container mt-4">
+        <Routes>
+          <Route path="/mascotas/*" element={<MascotaPage />} />
+
+          <Route
+            path="/"
+            element={<Navigate to="/mascotas/listar" />}
+          />
+        </Routes>
+      </div>
+    </>
+  );
 }
 
-export default App
+export default App;
+
